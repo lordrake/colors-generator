1c993826f9fc89e8531904235af430b1fb1283c9
Danilo Colella <danilocolella17@gmail.com>
2022-05-07 12:33:56 +0200

    Updates

---
+++ precache-manifest.5f634ec620ba979dfa1eb554369c33e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1cdec25a78439687bde1fd9b5d5cb2",
+    "url": "/colors-generator/index.html"
+  },
+  {
+    "revision": "1cf642434d727e8c5642",
+    "url": "/colors-generator/static/css/main.ecf02f8c.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/colors-generator/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/colors-generator/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf642434d727e8c5642",
+    "url": "/colors-generator/static/js/main.1b3fe324.chunk.js"
+  },
+  {
+    "revision": "32e2b94cafcb5e5adb9b",
+    "url": "/colors-generator/static/js/runtime-main.bf110e62.js"
+  }
+]);
